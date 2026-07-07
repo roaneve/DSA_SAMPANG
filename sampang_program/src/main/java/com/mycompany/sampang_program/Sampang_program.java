2caa3a972a42cc9af553bdbda397b2b4f07ff7aa
markhexcore <markregieverzon0818@gmail.com>
2026-07-07 08:54:38 +0800

    ACT2A firstprogram

---
+++ sampang_program/src/main/java/com/mycompany/sampang_program/Sampang_program.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sampang_program;
+import java.util.Scanner;
+/**
+ *
+ * @author STUDENT
+ */
+public class Sampang_program {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello World!");
+        System.out.println("Sampang Roan!");
+        System.out.println("number!");
+        int x = input.nextInt();
+       System.out.println("number!");
+        int y = input.nextInt();
+        
+        int sum;
+        sum = x + y;
+        
+         System.out.println("sum is :"+ sum);
+    }
+}
